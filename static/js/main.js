667443c330dfec1850137d7d32b0d1493600194a
mrmnmly <lukaszkups@gmail.com>
2016-11-06 18:47:24 +0100

    added doctype, charset, and modal js functions

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,56 @@
 (function(){
+	// helper function for opening modal window
+	function openModal(modalId){
+		var modal = document.getElementById(modalId);
+		var wrapper = document.querySelector('.modal-wrapper');
+		if(modal && wrapper){
+			wrapper.style.display = 'block';
+			modal.style.display = 'block';
+		}else{
+			console.warn('Modal or modal-wrapper DOM elemenent not found.');
+		}
+	}
+
+	// helper function for closing modal window
+	function closeModal(modalId){
+		if(modalId){
+			var modal = document.getElementById(modalId);
+			var wrapper = document.querySelector('.modal-wrapper');
+			if(modal && wrapper){
+				wrapper.style.display = 'none';
+				modal.style.display = 'none';
+			}else{
+				console.warn('Modal or modal-wrapper DOM elemenent not found.');
+			}
+		}else{
+			document.querySelector('.modal-wrapper').style.display = 'none';
+			var modals = document.querySelectorAll('.modal-wrapper > *');
+			for(var x = 0, l = modals.length; x < l; x++){
+				modals[x].style.display = 'none';
+			}
+		}
+		
+	}
+
+	// helper function that binds click event to modal wrapper that closes modal
+	function bindModalWrapperClick(){
+		document.querySelector('.modal-wrapper').addEventListener('click', function(e){
+			e.preventDefault();
+			closeModal();
+		});
+	}
+	bindModalWrapperClick();
+
+	// nice scroll plugin initialization
 	$('.list-container, .categories').niceScroll({
 		cursorcolor: "#e52325",
 		cursorborder: "none",
 		cursorborderradius: 0
+	});
+
+	// bind info-button click to show modal
+	document.getElementById('info-button').addEventListener('click', function(e){
+		e.preventDefault();
+		openModal('info-modal');
 	});
 })();
